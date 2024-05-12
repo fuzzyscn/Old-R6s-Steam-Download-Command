d112ed7ef9d00a04c2d9c5f0e6d78d6f397c8267
“fuzzyscn” <2561417364@qq.com>
2024-05-12 20:45:59 +0800

    2024.4.15 更新1.2修复大写Y不能表示同意，增加两个免密下载赛季 startGame.lower()（代码已遗失） 2024.4.21 下载器更新1.3 解决下载完以后输y无法自动启动游戏 subprocess.run 增加执行路径 cwdPath

--- lib/python-3.12.3-embed-amd64/_r6downloader.py
+++ lib/python-3.12.3-embed-amd64/_r6downloader.py
@@ -14,14 +14,14 @@ manifestList = {
     "Y1S4" : ["r6_y1s4_377237", "r6_y1s4_359551", "3576607363557872807 8569920171217002292",0, "赤鸦行动 | 28.5 GB ", "Plazas\\PLAZA_BO", 7, 0, "RainbowSix.exe"],
     "Y2S1" : ["r6_y2s1_377237", "r6_y2s1_359551", "2248734317261478192 8006071763917433748",1, "丝绒壳行动 | 33.2 GB ", "Plazas\\PLAZA_NEW", 7, 0, "RainbowSix.exe"],
     "Y2S2" : ["r6_y2s2_377237", "r6_y2s2_359551", "5875987479498297665 708773000306432190",1, "健康行动 | 34 GB ", "Plazas\\PLAZA_NEW", 7, 0, "RainbowSix.exe"],
-    "Y2S3" : ["r6_y2s3_377237", "r6_y2s3_359551", "6708129824495912434 1613631671988840841",1, "血兰花行动 | 34.3 GB ", "Plazas\\PLAZA_NEW", 7, 0, "RainbowSix.exe"],
+    "Y2S3" : ["r6_y2s3_377237", "r6_y2s3_359551", "6708129824495912434 1613631671988840841",0, "血兰花行动 | 34.3 GB ", "Plazas\\PLAZA_NEW", 7, 0, "RainbowSix.exe"],
     "Y2S4" : ["r6_y2s4_377237", "r6_y2s4_359551", "8748734086032257441 4221297486420648079",1, "白噪声行动 | 48.7 GB ", "Plazas\\PLAZA_NEW", 7, 0, "RainbowSix.exe"],
     "Y3S1" : ["r6_y3s1_377237", "r6_y3s1_359551", "5071357104726974256 4701787239566783972",0, "奇美拉行动 | 58.8 GB ", "Plazas\\PLAZA_NEW", 7, 0, "RainbowSix.exe"],
     "Y3S2" : ["r6_y3s2_377237", "r6_y3s2_359551", "6507886921175556869 8765715607275074515",1, "备战行动 | 63.3 GB ", "Plazas\\PLAZA_NEW", 7, 0, "RainbowSix.exe"],
     "Y3S3" : ["r6_y3s3_377237", "r6_y3s3_359551", "5562094852451837435 7781202564071310413",1, "暗空行动 | 72.6 GB ", "Plazas\\PLAZA_NEW", 7, 0, "RainbowSix.exe"],
     "Y3S4" : ["r6_y3s4_377237", "r6_y3s4_359551", "6502258854032233436 7659555540733025386",1, "风城行动 | 76.9 GB ", "Plazas\\PLAZA_NEW", 7, 0, "RainbowSix.exe"],
     "Y4S1" : ["r6_y4s1_377237", "r6_y4s1_359551", "8356277316976403078 5935578581006804383",1, "燃烧地平线 | 59.7 GB ", "Plazas\\PLAZA_NEW", 7, 0, "RainbowSix.exe"],
-    "Y4S2" : ["r6_y4s2_377237", "r6_y4s2_359551", "693082837425613508 5408324128694463720",1, "幻镜行动 | 67.1 GB ", "Plazas\\PLAZA_NEW", 7, 0, "RainbowSix.exe"],
+    "Y4S2" : ["r6_y4s2_377237", "r6_y4s2_359551", "693082837425613508 5408324128694463720",0, "幻镜行动 | 67.1 GB ", "Plazas\\PLAZA_NEW", 7, 0, "RainbowSix.exe"],
     "Y4S3" : ["r6_y4s3_377237", "r6_y4s3_359551", "3546781236735558235 7869081741739849703",1, "余烬重燃行动 | 69.6 GB ", "Plazas\\PLAZA_NEW", 7, 0, "RainbowSix.exe"],
     "Y4S4" : ["r6_y4s4_377237", "r6_y4s4_359551", "299124516841461614 1842268638395240106",0, "幻变潮汐行动（全皮肤）| 75.2 GB ", "Plazas\\PLAZA_NEW", 7, 0, "RainbowSix.exe"],
     "Y5S1" : ["r6_y5s1_377237", "r6_y5s1_359551", "4736360397583523381 6296533808765702678",1, "虚空边境行动 | 74.3 GB ", "Plazas\\PLAZA_NEW", 7, 0, "RainbowSix.exe"],
@@ -85,14 +85,14 @@ def DownloadConfirm(_path, _version, _gamePath, txt):
         else:
             input("\n回车开始下载彩虹六号 "+_version+" "+manifestList[_version][4]+" 启动文件为：" + manifestList[_version][8])
 
-def RunGame(path):
+def RunGame(filePath, cwdPath):
     try:
-        subprocess.run(['start', path], shell=True)
+        subprocess.run(['start', filePath], shell=True, cwd=cwdPath)
     except Exception as e:
         print(e)
         
 def DownloadPatch(_version, _gamePath):
-    name = input("\n下载验证完毕，为避免存档冲突及联机使用，请设置你的英文游戏昵称：")
+    name = input("\n下载验证完毕，为避免存档冲突及联机使用，请输入你的英文游戏昵称（不要直接回车！）：")
     name += '-'+_version
     patchFileCount = 0
     patchFolderCount = 0
@@ -130,23 +130,32 @@ def DownloadPatch(_version, _gamePath):
         if os.path.exists(startGamePath):
             print("\n游戏启动路径为: "+startGamePath)
             startGame = input("\n启动游戏的 exe 文件已准备好，是否启动游戏？(y/n)：")
+            startGame = startGame.lower()
             if startGame == "y":
-                RunGame(startGamePath)
+                RunGame(startGamePath, _gamePath)
+            else:
+                print("\n下载已成功！即将返回主菜单！请手动启动游戏。\n")
         else:
             startGamePath = os.path.join(_gamePath, "RainbowsixGame.exe")
             if os.path.exists(startGamePath):
                 print("\n游戏启动路径为: "+startGamePath)
                 startGame = input("\n启动游戏的 exe 文件已准备好，是否启动游戏？(y/n)：")
+                startGame = startGame.lower()
                 if startGame == "y":
-                    RunGame(startGamePath)
+                    RunGame(startGamePath, _gamePath)
+                else:
+                    print("\n下载已成功！即将返回主菜单！请手动启动游戏。\n")
             else:
                 print("\n没有启动游戏的 exe 文件！请重新下载以验证游戏完整性！")
     else:
         if os.path.exists(startGamePath):
             print("\n游戏启动路径为: "+startGamePath)
             startGame = input("\n启动游戏的 bat 文件已准备好，是否启动游戏？(y/n)：")
+            startGame = startGame.lower()
             if startGame == "y":
-                RunGame(startGamePath)
+                RunGame(startGamePath, _gamePath)
+            else:
+                print("\n下载已成功！即将返回主菜单！请手动启动游戏。\n")
         else:
             print("\n没有启动游戏的 bat 文件！请重新下载以验证游戏完整性！")
 
@@ -171,6 +180,7 @@ def Main():
         DownloadPatch(_version, _gamePath)
     elif gongNeng == "2":
         UUjiaoCheng = input("是否已使用UU加速器 路由模式 加速Steam商店？(y/n)：")
+        UUjiaoCheng = UUjiaoCheng.lower()
         if UUjiaoCheng != "y":
             os.system('lib\\UU.png')
             
@@ -201,17 +211,20 @@ def Main():
         DownloadPatch(_version, _gamePath)
     elif gongNeng == "3":
         y5 = input("\n你玩的版本是否是Y5？(y/n)：")
+        y5 = y5.lower()
         if y5 == "y":
             subprocess.run(['start', "lib\\Y5_xiu_gai_qi.exe"], shell=True)
         else:
             subprocess.run(['start', "lib\\R6_Liberator_0.0.0.22.exe"], shell=True)
     elif gongNeng == "4":
         installVpn = input("\n是否安装虚拟局域网软件OpenVPN用于联机？(y/n)：")
+        installVpn = installVpn.lower()
         if installVpn == "y":
             subprocess.run(['start', "lib\\openvpn-install-2.4.8-I602-Win10.exe"], shell=True)
             print("安装中... ... \n安装完毕后到QQ群：439523286 中下载联机节点文件client50120.ovpn导入既可连接群内联机服务器！")
     elif gongNeng == "5":
         y5y8 = input("\n你联机的版本是否是Y5-Y8？(y/n)：")
+        y5y8 = y5y8.lower()
         hostIP = input("\n请输入房主的虚拟局域网IP：")
         if y5y8 == "y":
             os.system("lib\\NetworkedR6.exe "+hostIP+" -p 6200")
@@ -220,9 +233,8 @@ def Main():
     elif gongNeng == "6":
         os.system("route print -4")
     else:
-        Help()
         print('\n                ----请输入提示的功能编号！！！')
-        
+    Help()    
     Main()
     
 if __name__ == "__main__":
